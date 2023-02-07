20d9996a4c428e9b3cc11ff7b98769e5e1709e55
timabloom <timabloom@proton.me>
2023-02-07 10:28:02 +0100

    added throw error to createRoot

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+/**
+ * @param {HTMLElement} rootElement
+ *   The root element of the React application, obtained by `document.getElementById("root")`.
+ *   It should be a valid HTML element.
+ */
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<App />);
+} else {
+  throw new Error("Root element with id 'root' not found in the DOM");
+}
