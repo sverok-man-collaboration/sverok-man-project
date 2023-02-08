b29ee5906f32dff678cd7d287f207972c3850954
Saga Vallin <saga.vallin@gmail.com>
2023-02-08 13:52:51 +0100

    added font and colors to tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,19 @@ content: [
   "./src/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
+    fontFamily: {
+      header: ["DocumanHeavy", "sans-serif"],
+      body: ["Open sans", "sans-serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      primary: "#451499",
+      secondary: "#c8b2ff",
+      black: "#2d2d2d",
+      grey: "#d9dbda",
+      white: "#ffffff",
+      accent: "#f7528b",
+    },
     extend: {},
   },
   plugins: [],
