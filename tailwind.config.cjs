1b817e84344905e8072d0685e43cb39f246778fe
Saga Vallin <saga.vallin@gmail.com>
2023-02-09 12:44:44 +0100

    Revert "added media queries in tailwind theme"
    This reverts commit dc8bb74d739ad3b7f9313ca3bfb0fd045c488ad5.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-
-import defaultTheme from "tailwindcss/defaultTheme";
-
 module.exports = {
 content: [
   "./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
-    screens: {
-      xs: "280px",
-      ...defaultTheme.screens,
-    },
     fontFamily: {
       header: ["DocumanHeavy", "sans-serif"],
       body: ["Open sans", "sans-serif"],
