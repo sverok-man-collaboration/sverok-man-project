683d89b4d3cef7e3b4735a76e7833b4c0bd2a15b
timabloom <timabloom@proton.me>
2023-02-10 11:38:48 +0100

    created a link to the about page

--- src/pages/Home/components/AboutLink.jsx
+++ src/pages/Home/components/AboutLink.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function AboutLink() {
-  return <></>;
+  return (
+    <div className="flex justify-center">
+      <Link to="about">Om detta projekt</Link>
+    </div>
+  );
 }
 
 export default AboutLink;
