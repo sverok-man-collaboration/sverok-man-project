631763ba83e5035f0ad64e8fe980a83d806bc9ad
SofiaDrejmark <sofiadrejmark@gmail.com>
2023-02-13 12:30:03 +0100

    added styling

--- src/pages/Home/components/AboutLink.jsx
+++ src/pages/Home/components/AboutLink.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 
 function AboutLink() {
   return (
-    <div className="flex justify-center">
-      <Link to="about">Om detta projekt</Link>
+    <div className="flex justify-center items-center mt-10 flex-col">
+      <h5 className="text-xl">Vill du läsa mer om projektet? </h5>
+      <Link to="about">
+        <button className="bg-accent text-white hover:bg-accentHover m-4 rounded-full text-lg px-6 py-2">Tryck här!</button>
+      </Link>
     </div>
   );
 }
