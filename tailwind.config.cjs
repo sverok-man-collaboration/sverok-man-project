c57634a19973de188c726d3fcaf6486bdf2285e7
timabloom <timabloom@proton.me>
2023-02-15 12:26:02 +0100

    formated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-
-content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
       xs: "280px",
@@ -24,10 +20,10 @@ content: [
       black: "#2d2d2d",
       grey: "#d9dbda",
       white: "#ffffff",
-      accent: "#f7528b", 
+      accent: "#f7528b",
       accentHover: "#F41A65",
     },
     extend: {},
   },
   plugins: [],
-}
+};
